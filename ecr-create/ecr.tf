d7a1ada9ddde81ea6eee60bb6d2abcbb1dc376e4
nwaykole <80094560+nwaykole@users.noreply.github.com>
2021-11-26 19:31:34 +0000

    Update ecr.tf

--- ecr-create/ecr.tf
+++ ecr-create/ecr.tf
@@ -1,5 +1,5 @@
 resource "aws_ecr_repository" "Pyflaskrepo" {
-  name                 = "Pyflaskrepo"
+  name                 = "pyflaskrepo"
   image_tag_mutability = "MUTABLE"
 
   image_scanning_configuration {
