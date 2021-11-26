c26d04e4416c78e47cd4d7687bd98e283fe40909
nwaykole <80094560+nwaykole@users.noreply.github.com>
2021-11-26 20:57:08 +0000

    Update network.tf

--- ecs-deploy/network.tf
+++ ecs-deploy/network.tf
@@ -28,6 +28,14 @@ resource "aws_security_group" "ELB-sg" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+  
+  ingress {
+    description = "HTTP from VPC"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
   egress {
     from_port   = 0
